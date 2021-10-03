9878737bf945aff94cca0d5b078b3219098d962d
kyusang0330 <cholongs123@naver.com>
2021-10-03 17:43:49 +0900

    10869

--- 10869.js
+++ 10869.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().split(" ");
+var input = fs.readFileSync('data.txt').toString().split(" ");
 
 var A = parseInt(input[0]);
 var B = parseInt(input[1]);
@@ -7,5 +7,5 @@ var B = parseInt(input[1]);
 console.log(A+B);
 console.log(A-B);
 console.log(A*B);
-console.log(A/B);
+console.log(parseInt(A/B));    // 문자열을 정수로 바꾸어 주는 함수 praseInt()를 사용해야 한다.
 console.log(A%B);
